e45055a16c08b524cd014bff2505ce7d0e0ae3f0
Paavveel <pa.tomilin@gmail.com>
2022-10-14 10:47:48 +0300

    feat: add pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import { FavoritesPage } from './pages/FavoritesPage/FavoritesPage';
+import { HomePage } from './pages/HomePage/HomePage';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='favorites' element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
