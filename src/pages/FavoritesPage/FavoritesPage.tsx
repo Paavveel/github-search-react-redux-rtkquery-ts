61dfb216600e04f1c8c591b0a49ff3e823fabc43
Pavel T <pa.tomilin@gmail.com>
2022-10-16 15:16:12 +0300

    feat: add structure/data fro the Favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,3 +1,21 @@
+import { useAppSelector } from '../../app/hooks';
+import { selectFavorites } from '../../features/github/githubSlice';
+
 export const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const favorites = useAppSelector(selectFavorites);
+
+  if (favorites.length === 0) return <p className='text-center'>No items.</p>;
+  return (
+    <div className='flex justify-center pt-10 mx-auto'>
+      <ul className='list-none'>
+        {favorites.map((fav) => (
+          <li key={fav}>
+            <a href={fav} target='_blank' rel='noopener noreferrer'>
+              {fav}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
